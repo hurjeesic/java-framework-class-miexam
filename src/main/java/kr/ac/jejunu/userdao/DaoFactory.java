76a24f34d5d1baad9126bf547b1c3fda7ea127da
Jimen <hurjeesic@naver.com>
2020-05-08 15:53:01 +0900

    문제 6 : 각자가 만든 Connection 정보관리 Object 를 DataSource 로 변경해보세요.

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,15 +1,42 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 public class DaoFactory {
+    @Value("${db.classname}")
+    private String className;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
     @Bean
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        return new UserDao(datasource());
     }
 
     @Bean
-    private JejuConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    private DataSource datasource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        try {
+            dataSource.setDriverClass((Class<? extends Driver>)Class.forName(className));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
     }
 }
